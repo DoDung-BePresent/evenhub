5bfd6834777c941850c76671ac8cac66f9da2712
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2025-12-13 13:43:17 +0700

    feat: implement authentication flow with AsyncStorage; add access token handling in App, LoginScreen, and HomeScreen

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
   return (
     <View>
       <Text>LoginScreen</Text>
-    </View>
-  )
-}
 
-export default LoginScreen
+      <Button
+        title="Login"
+        onPress={async () =>
+          await AsyncStorage.setItem('accessToken', 'fafafa')
+        }
+      />
+    </View>
+  );
+};
+
+export default LoginScreen;
