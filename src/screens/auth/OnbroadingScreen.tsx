fb2decd463a18c1c67275a4e882215d253793660
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2025-12-15 17:25:46 +0700

    feat: add custom button and text components with font integration
    - Created ButtonComponent for reusable button UI with icon support.
    - Developed TextComponent for consistent text styling across the app.
    - Added fontFamilies constant to manage custom fonts.
    - Configured asset linking for fonts in react-native.config.js.
    - Generated link-assets-manifest.json for font asset management.

--- src/screens/auth/OnbroadingScreen.tsx
+++ src/screens/auth/OnbroadingScreen.tsx
@@ -4,6 +4,8 @@ import { globalStyles } from '../../styles/globalStyles';
 import Swiper from 'react-native-swiper';
 import { appInfo } from '../../constants/appInfos';
 import { appColors } from '../../constants/appColors';
+import { TextComponent } from '../../components';
+import { fontFamilies } from '../../constants/fontFamilies';
 
 const OnboardingScreen = ({ navigation }: any) => {
   const [index, setIndex] = useState(0);
@@ -60,14 +62,22 @@ const OnboardingScreen = ({ navigation }: any) => {
         ]}
       >
         <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
-          <Text style={[styles.text, { color: appColors.gray2 }]}>Skip</Text>
+          <TextComponent
+            color={appColors.gray2}
+            text="Skip"
+            font={fontFamilies.medium}
+          />
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() =>
             index < 2 ? setIndex(index + 1) : navigation.navigate('LoginScreen')
           }
         >
-          <Text style={[styles.text]}>Next</Text>
+          <TextComponent
+            color={appColors.white}
+            text="Next"
+            font={fontFamilies.medium}
+          />
         </TouchableOpacity>
       </View>
     </View>
